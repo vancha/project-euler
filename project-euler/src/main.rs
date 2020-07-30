dc057219f290e8c35f0ee2ad8a6c61fe3f5e40f4
Vancha <tjipke@tutanota.com>
2020-07-30 10:33:46 +0200

    Added a buttload of comments

--- project-euler/src/main.rs
+++ project-euler/src/main.rs
@@ -1,6 +1,14 @@
-mod hello;
+///module that contains all the solutions to project eulers problems.
+///
+/// # Description
+///
+/// inside this module, there is a method/function for each solved problem, in the form of problem_XXX, where XXX is the corresponding number from the project euler site.
+/// There are some functions that are usefull to multiple problems, these should eventually be put in some sort of lib.rs library file.
+mod problems;
 
+
+///used only to select a single problem from the problems module
 fn main() {
-    hello::problem_016();
+    problems::problem_017();
 
 }
